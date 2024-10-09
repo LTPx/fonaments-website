b34ed8d5dc7382a3d0cf9b02b82d7765ab23dab1
Lenin Capa <leviskp87@gmail.com>
2024-10-08 20:55:51 -0500

    initial commit

---
+++ src/app/[locale]/layout.tsx
@@ -0,0 +1,48 @@
+import { NextIntlClientProvider } from "next-intl";
+import { getMessages } from "next-intl/server";
+import "./globals.scss";
+import "tailwindcss/tailwind.css";
+import "../global.css"
+import { Metadata } from "next";
+import App from "./app";
+
+
+export async function generateMetadata({
+  params: { locale },
+}: {
+  params: { locale: string };
+}): Promise<Metadata> {
+  return {
+    title: "Fonaments",
+    description: "New Site",
+    // robots: seoData.robots,
+    openGraph: {
+      title: "Fonaments",
+      description: "New Site",
+      siteName: "",
+      locale: locale,
+    },
+  };
+}
+
+export default async function LocaleLayout({
+  children,
+  params: { locale },
+}: {
+  children: React.ReactNode;
+  params: { locale: 'en' | 'es' | 'de' };
+}) {
+  const messages = await getMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider messages={messages}>
+          <App locale={locale}>
+            {children}
+          </App>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/_services/api.ts
@@ -0,0 +1,17 @@
+import { WordPressFrontendPage } from "../_interfaces/wordpress-page";
+
+export async function getWordPressPage(
+  locale: "en" | "es" | "de",
+  page: string
+): Promise<WordPressFrontendPage> {
+  const url = `${process.env.WORDPRESS_API_URL}/wp/v2/pages/${page}`;
+  console.log("url: ", url);
+  const response = await fetch(url, {
+    next: {
+      revalidate: 0,
+    },
+  });
+  const dataJson = await response.json();
+  if (!response.ok) throw new Error(dataJson.message);
+  return dataJson;
+}
