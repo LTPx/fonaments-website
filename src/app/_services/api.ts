82f35689322eae781ab83102b20f5b472883d8c6
Lenin Capa <leviskp87@gmail.com>
2024-10-22 20:18:20 -0500

    get projects api

--- src/app/_services/api.ts
+++ src/app/_services/api.ts
@@ -1,3 +1,5 @@
+import { WordPressPost } from "../_interfaces/wordpress";
+import { ProjectPostWp, ProjectWp } from "../_interfaces/wordpress-components";
 import { WordPressFrontendPage } from "../_interfaces/wordpress-page";
 
 export async function getWordPressPage(
@@ -16,4 +18,20 @@ export async function getWordPressPage(
   if (!response.ok) throw new Error(pages.message);
   const page = pages.length ? pages[0] : undefined;
   return page;
+}
+
+export async function getAllProjects(
+  locale: "en" | "es" | "de",
+): Promise<WordPressPost[]> {
+  const WORDPRESS_API_URL= 'http://www.staging.fonamentsarch.com/wp-json';
+  const url = `${WORDPRESS_API_URL}/wp/v2/all_projects?_embed`;
+  console.log("url projects: ", url);
+  const response = await fetch(url, {
+    next: {
+      revalidate: 0,
+    },
+  });
+  const projects = await response.json();
+  if (!response.ok) throw new Error(projects.message);
+  return projects;
 }
--- src/app/components/header-carousel.tsx
+++ src/app/components/header-carousel.tsx
@@ -1,21 +1,32 @@
-import Link from "next/link";
+import { Link } from "@/navigation";
 import Carousel from "./carousel";
-import { ProjectPostWp, ProjectWp } from "../_interfaces/wordpress-components";
+import { ProjectWp } from "../_interfaces/wordpress-components";
+import { getAllProjects } from "../_services/api";
 
 interface HeaderCarouselProps {
   items: ProjectWp[];
 }
 
-export function HeaderCarousel(props: HeaderCarouselProps) {
+export async function HeaderCarousel(props: HeaderCarouselProps) {
   const { items } = props;
+  const allProjects = await getAllProjects("en");
+  const projectsIdsSelected = items.map((item) => item.project.ID);
+  const projects = allProjects.filter((project) => {
+    return projectsIdsSelected.includes(project.id);
+  });
 
   return (
     <Carousel slidesNumber={3}>
-      {items.map((item, index) => (
-        <div key={index}>
-          <img src={item.project.imageUrl} className="h-[456px] xl:h-[600px] object-cover w-full" />
-          <p className="font-medium text-[14px] leading-[26px]">{item.project.post_title}</p>
-        </div>
+      {projects.map((item, index) => (
+        <Link href={`/projects/${item.slug}`} key={index}>
+          <img
+            src={item._embedded["wp:featuredmedia"][0].source_url}
+            className="h-[456px] xl:h-[600px] object-cover w-full"
+          />
+          <p className="font-medium text-[14px] leading-[26px]">
+            {item.title.rendered}
+          </p>
+        </Link>
       ))}
     </Carousel>
   );
