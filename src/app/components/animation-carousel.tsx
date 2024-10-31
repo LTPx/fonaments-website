1cacfe635131c5c3ccce7b92c300533e78c860e6
Ricardo Capa <ricardocmdn@gmail.com>
2024-10-30 21:38:13 -0500

    swiper carousel

--- src/app/components/animation-carousel.tsx
+++ src/app/components/animation-carousel.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { Link } from "@/navigation";
-import Carousel from "./carousel";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Mousewheel } from "swiper/modules";
 import ProjectCard from "./project-card";
 import { WordPressPost } from "../_interfaces/wordpress";
 import { useEffect, useState } from "react";
+import "swiper/swiper-bundle.css";
 
 interface AnimationCarouselProps {
   projects: WordPressPost[];
@@ -13,37 +15,38 @@ interface AnimationCarouselProps {
 export function AnimationCarousel(props: AnimationCarouselProps) {
   const { projects } = props;
   const [isVisible, setIsVisible] = useState(false);
-  const [isCarouselFinished, setIsCarouselFinished] = useState(false);
 
   useEffect(() => {
     setIsVisible(true);
   }, []);
 
-  useEffect(() => {
-    if (isCarouselFinished) {
-      document.body.style.overflow = "auto";
-    } else {
-      document.body.style.overflow = "hidden";
-    }
-  }, [isCarouselFinished]);
-
   return (
     <div className="relative z-40">
-      <Carousel
-        slidesNumber={3}
-        slidesToScroll={3}
-        onFinish={() => setIsCarouselFinished(true)}
+      <Swiper
+        modules={[Navigation, Mousewheel]}
+        spaceBetween={15}
+        navigation={{ nextEl: ".arrow-right", prevEl: ".arrow-left" }}
+        mousewheel={true}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+        }}
+        loop={false}
+        speed={1000}
+        className={`transition-opacity duration-700 ${
+          isVisible ? "opacity-100" : "opacity-0"
+        }`}
       >
         {projects.map((item, index) => (
-          <Link href={`/projects/${item.slug}`} key={index}>
-            <div
-              className={`transition-opacity duration-700 ${
-                isVisible ? "opacity-100" : "opacity-0"
-              }`}
-              style={{
-                transitionDelay: `${index * 300}ms`,
-              }}
-            >
+          <SwiperSlide key={index}>
+            <Link href={`/projects/${item.slug}`}>
               <ProjectCard
                 className="h-[456px] lg:h-[50vh] xl:h-[52vh]"
                 title={item.title.rendered}
@@ -52,10 +55,35 @@ export function AnimationCarousel(props: AnimationCarouselProps) {
                   "https://s3-alpha-sig.figma.com/img/b0ec/1da8/205f0bec1ed743c9339f5dc9d99bb8e9?Expires=1730678400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=NhcIzXBMUi8I10tWwPeCV-7SnXiEEWlil1kByWeJwAvW6Go8OdGINKhq6DnFb6vajTT~~yI0yXMVTDdrnMm~rQXzpFjt603UHXNsTbSXmuv~NZoFDVb9NtyL0~btkFw9iOgzGZzP2OMeh60V49-XYVIq98MCKrlyQcbyr~jGAEPWreinUczNftiiTrPawOyoKxaHSB~xFjZdcqixBH1aClRSH~p0YRjumHiWJw7MLHAeLAwpAcvQcNF-O-7AFePVS1YawDQ731qqI33Wr9XtRXgUHzbYMzHbOv7cdRRudYZwoNLbqln9us9xmsXo3j7GwpLZSAKZF7S6LUwynBfb8g__"
                 }
               />
-            </div>
-          </Link>
+            </Link>
+          </SwiperSlide>
         ))}
-      </Carousel>
+        <div className="flex justify-between">
+          <button className="arrow-left arrow">
+            <img
+              src="/images/icons/arrow-left.svg"
+              style={{
+                position: "relative",
+                width: "45px",
+                zIndex: 500,
+              }}
+            />
+          </button>
+          <div className="carousel-right">
+            <button className="arrow-right arrow ">
+              <img
+                src="/images/icons/arrow-right.svg"
+                style={{
+                  right: "10px",
+                  position: "relative",
+                  width: "45px",
+                  zIndex: 500,
+                }}
+              />
+            </button>
+          </div>
+        </div>
+      </Swiper>
     </div>
   );
 }
--- src/app/components/header-carousel.tsx
+++ src/app/components/header-carousel.tsx
@@ -1,23 +1,45 @@
+"use client";
+
 import { ProjectWp } from "../_interfaces/wordpress-components";
 import { getAllProjects } from "../_services/api";
 import MotionLogo from "./motion-logo";
 import AnimationCarousel from "./animation-carousel";
+import { useEffect, useState } from "react";
+import { WordPressPost } from "../_interfaces/wordpress";
 
 interface HeaderCarouselProps {
   items: ProjectWp[];
 }
 
-export async function HeaderCarousel(props: HeaderCarouselProps) {
+export function HeaderCarousel(props: HeaderCarouselProps) {
   const { items } = props;
-  const allProjects = await getAllProjects("en");
-  const projectsIdsSelected = items.map((item) => item.project.ID);
-  const projects = allProjects.filter((project) => {
-    return projectsIdsSelected.includes(project.id);
-  });
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [projects, setProjects] = useState<WordPressPost[]>([]);
+
+  useEffect(() => {
+    async function fetchProjects() {
+      const allProjects = await getAllProjects("en");
+      const projectsIdsSelected = items.map((item) => item.project.ID);
+      const filteredProjects = allProjects.filter((project) =>
+        projectsIdsSelected.includes(project.id)
+      );
+      setProjects(filteredProjects);
+      setIsLoading(false);
+    }
+
+    fetchProjects();
+  }, [items]);
 
   return (
     <div className="relative flex flex-col lg:h-[100vh]">
-      <AnimationCarousel projects={projects}/>
+      {/* <div>
+      {isLoading && (
+        <div className="fixed inset-0 bg-body z-50 flex items-center justify-center">
+        </div>
+      )}
+      </div> */}
+      <AnimationCarousel projects={projects} />
       <MotionLogo />
     </div>
   );
