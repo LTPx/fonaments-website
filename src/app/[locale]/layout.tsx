0d6ae6740d76857f8ae7c28d2b7745602fb9d375
Angel Capa <ricardocmdn@gmail.com>
2025-03-09 21:40:05 -0500

    add logo icon

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -15,6 +15,9 @@ export async function generateMetadata({
   return {
     title: "Fonaments",
     description: "New Site",
+    icons: {
+      icon: "/images/logo.png",
+    },
     // robots: seoData.robots,
     openGraph: {
       title: "Fonaments",
